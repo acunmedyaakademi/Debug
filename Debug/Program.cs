ca78e4c2571c70dfb7d105f9b5e20dfcd5a9206a
Ceassur <140329797+Ceassur@users.noreply.github.com>
2023-10-06 23:26:37 +0300

    Debug Yüklendi

---
+++ Debug/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Debug
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 8;
+            int num3 = 0;
+
+            num3 = num2/num1;
+            Console.WriteLine(num3);
+            Console.ReadKey();
+        }
+    }
+}
